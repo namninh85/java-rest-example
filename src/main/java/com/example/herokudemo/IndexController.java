b3d5d7ff33b99c2bfeba4759c18944e6659d5a6c
namninh85 <namninh85@gmail.com>
2020-02-04 09:10:58 +0700

    Update IndexController.java

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -2,6 +2,7 @@ package com.example.herokudemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class IndexController {
@@ -9,5 +10,24 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "Hello there! I'm running.";
+    }
+    
+    @GetMapping("/test")
+        public String error(HttpServletRequest request) {
+            return getClientIp(request)
+        }
+    
+    private static String getClientIp(HttpServletRequest request) {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        return remoteAddr;
     }
 }
